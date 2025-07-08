a807568d89c641471ad70397134ac9393bf25db9
Brandon Luna <brandonluna934@gmail.com>
2025-07-08 10:32:32 -0600

    Video 6

---
+++ condiciones.py
@@ -0,0 +1,9 @@
+x = int(input("x: "))
+y = int(input("y: "))
+
+if x < y:
+    print("x es menor que y")
+elif x > y:
+    print("x es mayor que y")
+else:
+    print("x es igual a y")
---
+++ saludo.py
@@ -0,0 +1,6 @@
+import sys
+
+if len(sys.argv) == 2:
+    print(f"Hola, {sys.argv[1]}")
+else:
+    print("Uso: python hola.py [nombre]")
